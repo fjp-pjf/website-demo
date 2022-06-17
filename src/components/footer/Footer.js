0c535b4953c1496a1ee1c54d31c3b49f540c972f
fjp-pjf <feminjustin07@gmail.com>
2022-06-17 21:47:57 +0530

    footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <Row className="footer-rights mx-0">
+        <p>© 2021 Alba. All Rights Reserved.</p>
+      </Row>
+    </>
+  );
+};
+
+export default Footer;
