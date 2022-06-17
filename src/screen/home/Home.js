0f3fd8b9da8cf98232dde2bace9e4334ae8892db
fjp-pjf <feminjustin07@gmail.com>
2022-06-17 21:48:32 +0530

    home

---
+++ src/screen/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
